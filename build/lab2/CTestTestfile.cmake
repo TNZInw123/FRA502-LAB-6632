5747e43ebbf99081790d4ba1443dc34e0b7ecf03
Pongnapat Arayangkul <54279858+TNZInw123@users.noreply.github.com>
2025-08-27 01:20:12 +0700

    Add files via upload

---
+++ build/lab2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tnzacer/FRA502-LAB-6632/src/lab2
+# Build directory: /home/tnzacer/FRA502-LAB-6632/build/lab2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
